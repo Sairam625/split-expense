f61b67f4f77e916531a0403f5be93cde9cf3d883
Nihar Hanagandi <nihar.hanagandi@gmail.com>
2026-01-11 19:06:35 +0530

    changedcopyright

--- client/src/components/Copyright.jsx
+++ client/src/components/Copyright.jsx
@@ -6,7 +6,7 @@ export default function Copyright() {
 
   return (
     <Typography mt="-2" variant="body2" align="center" sx={{ color: 'text.secondary'}}>
-            &copy; SAIRAM HATTI &nbsp; 
+            &copy; Nihar Hanagandi &nbsp; 
             <FavoriteIcon  color="error"  sx={{ fontSize: 18 ,mb:'-2px', ml:'-5px' }}/><br/>
             <Link variant="subtitle3" component={'a'} href="https://github.com/sairam625" target="_blank">
             [GitHub]
